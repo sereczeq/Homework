c8652262169e06d66d9b28bfe143f6c6a8ac495c
sereczeq <kubaserdynski+git@gmail.com>
2021-01-19 15:41:51 +0100

    mergesort cleanup

--- src/lab13/MergeSort.java
+++ src/lab13/MergeSort.java
@@ -1,14 +1,15 @@
 package lab13;
 
 import java.util.Arrays;
-import java.util.Vector;
 
 public class MergeSort {
-    int threads = 0;
-    final int maxThreads = 8;
-    int[] sort(int[] array){
-        if(array.length <= 1) return array;
-        else if (++threads <maxThreads) {
+    private int threads = 0;
+    private final int maxThreads = 8;
+
+    public int[] sort(int[] array)
+    {
+        if (array.length <= 1) return array;
+        else if (threads++ < maxThreads) {
             final int[][] arr1 = new int[1][1]; //IDE made this automatically, it works so thanks JetBrains
             final int[][] arr2 = new int[1][1];
             Thread t1 = new Thread(() -> arr1[0] = sort(split(array)[0]));
@@ -22,24 +23,21 @@ public class MergeSort {
                 e.printStackTrace();
             }
             return merge(arr1[0], arr2[0]);
-        }
-           else return merge(sort(split(array)[0]), sort(split(array)[1]));
+        } else return merge(sort(split(array)[0]), sort(split(array)[1]));
 
     }
 
-    static int[] merge(int[] array1, int[] array2)
-    {
+    private static int[] merge(int[] array1, int[] array2) {
         int[] newArray = new int[array1.length + array2.length];
-        for(int i = 0, arr1Counter = 0, arr2Counter = 0; i < newArray.length; i++)
-        {
-            if(arr2Counter >= array2.length || (arr1Counter < array1.length && array1[arr1Counter] < array2[arr2Counter])) newArray[i] = array1[arr1Counter++];
+        for (int i = 0, arr1Counter = 0, arr2Counter = 0; i < newArray.length; i++) {
+            if (arr2Counter >= array2.length || (arr1Counter < array1.length && array1[arr1Counter] < array2[arr2Counter]))
+                newArray[i] = array1[arr1Counter++];
             else newArray[i] = array2[arr2Counter++];
         }
         return newArray;
     }
 
-    static int[][] split(int[] array)
-    {
-        return new int[][]{Arrays.copyOf(array, array.length/2), Arrays.copyOfRange(array, array.length/2, array.length)};
+    private static int[][] split(int[] array) {
+        return new int[][]{Arrays.copyOf(array, array.length / 2), Arrays.copyOfRange(array, array.length / 2, array.length)};
     }
 }
